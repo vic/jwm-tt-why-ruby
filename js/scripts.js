848f65125be72aaa376d972a03ffb403adaf0ad1
Victor Hugo Borja <vborja@apache.org>
2010-10-25 10:13:54 -0500

    Added custom scripts

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+new(function($){
+
+  var initialize = function(){
+    $("#footer").append("<img id=ruby-logo src='http://github.com/vic/jwm-tt-why-ruby/raw/master/ruby-logo-512.png'>")
+    $("#footer").append("<img id=jwm-logo src='http://github.com/vic/jwm-tt-why-ruby/raw/master/jwm-logo.gif'>")
+  };
+
+  $(document).ready(initialize);
+
+})(jQuery);
